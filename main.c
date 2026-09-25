d6750a6f86b892812fdacf98777d68571486ec38
falllou12 <falloulo2004@icloud.com>
2026-09-25 11:20:51 +0200

    calculateur de moyenne en C

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float note1, note2, moyenne;
+
+    printf("=== CALCULATEUR DE MOYENNE ===\n");
+    
+    printf("Entrez la premiere note (/20) : ");
+    scanf("%f", &note1);
+
+    printf("Entrez la deuxieme note (/20) : ");
+    scanf("%f", &note2);
+
+    moyenne = (note1 + note2) / 2.0;
+
+    printf("\nVotre moyenne est de : %.2f/20\n", moyenne);
+
+    if (moyenne >= 10.0) {
+        printf("Resultat : Valide !\n");
+    } else {
+        printf("Resultat : Non valide, rattrapage necessaire.\n");
+    }
+
+    return 0;
+}
